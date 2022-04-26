830893cc068fe929690f00cf130f1fe568b0a92e
Matheus Mascarenhas <mattmascarenhas7@gmail.com>
2022-04-26 12:25:41 -0300

    software complete

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import './style.css';
+import api from "./services/api";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [cep, setCep] = useState({});
+
+  async function handleSearch() {
+    if (input === "") {
+      alert("Insira um CEP");
+    }
+
+    try {
+        const response = await api.get(`${input}/json`);
+        setCep(response.data);
+        setInput("");
+    } catch {
+        alert("Erro ao buscar");
+        setInput("");
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscar CEP...</h1>
 
       <div className="containerInput">
-        <input type="text" placeholder="Digite um CEP válido..."></input>
+        <input type="text"
+          placeholder="Digite um CEP válido..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}></input>
 
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#FFF"></FiSearch>
         </button>
       </div>
+      
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+        <h2>CEP: {cep.cep}</h2>
 
-      <main className="main">
-        <h2>CEP: 9598495</h2>
-
-        <spam>Rua Teste testando</spam>
-        <spam>Complemento: teste</spam>
-        <spam>Conjunto João Paulo II</spam>
-        <spam>Feira de Santana - BA</spam>
+        <spam>{cep.logradouro}</spam>
+        <spam>Complemento: {cep.complemento}</spam>
+        <spam>{cep.bairro}</spam>
+        <spam>{cep.localidade} - {cep.uf}</spam>
 
       </main>
+      )}
+      
 
     </div>
   );
